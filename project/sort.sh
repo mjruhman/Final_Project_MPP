cd05aa980123afd480bb9cba776e37bc88142f68
MJ <mj_ruhman1@baylor.edu>
2025-08-12 15:29:31 -0500

    a

--- project/sort.sh
+++ project/sort.sh
@@ -1,3 +1,3 @@
-/home/ruhmanm/code/project/sort 10000 0 0
-/home/ruhmanm/code/project/sort 10000 1 0
-/home/ruhmanm/code/project/sort 10000 2 0
+/home/ruhmanm/code/Final_Project/project/sort 10000 0 0
+/home/ruhmanm/code/Final_Project/project/sort 10000 1 0
+/home/ruhmanm/code/Final_Project/project/sort 10000 2 0
--- project2/sort.sh
+++ project2/sort.sh
@@ -1,3 +1,3 @@
-/home/ruhmanm/code/project2/sort 10000 0
-/home/ruhmanm/code/project2/sort 10000 1
-/home/ruhmanm/code/project2/sort 10000 2
+/home/ruhmanm/code/Final_Project/project2/sort 10000 0
+/home/ruhmanm/code/Final_Project/project2/sort 10000 1
+/home/ruhmanm/code/Final_Project/project2/sort 10000 2
